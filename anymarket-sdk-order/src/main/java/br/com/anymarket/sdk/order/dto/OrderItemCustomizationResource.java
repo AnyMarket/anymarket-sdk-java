c7e89f340671fb034619d483a05705bf85d166d0
joao.zanne <joao.zanne@db1.com.br>
2022-09-01 15:59:56 -0300

    Product Backlog Item 296064: [Netshoes] - Personalização de itens(via API)

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/OrderItemCustomizationResource.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/OrderItemCustomizationResource.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class OrderItemCustomizationResource {
+public class OrderItemCustomizationResource implements Serializable {
 
     @JsonProperty("item_customization_type")
     private String customizationType;
