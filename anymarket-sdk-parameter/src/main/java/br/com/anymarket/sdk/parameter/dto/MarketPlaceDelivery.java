dd53fc093d5bf9d0651b2a1cd77cba3291e810d7
Juliana Aline Petri Santos <juliana.santos@db1.com.br>
2022-10-31 14:33:01 -0300

    Product Backlog Item 323842: [TRAY]Garantir que os pedidos Amazon no fluxo DBA recebam o XML quando a importação automática de XML estiver configurada nos Parâmetros no ANY

--- anymarket-sdk-parameter/src/main/java/br/com/anymarket/sdk/parameter/dto/MarketPlaceDelivery.java
+++ anymarket-sdk-parameter/src/main/java/br/com/anymarket/sdk/parameter/dto/MarketPlaceDelivery.java
@@ -1,12 +1,13 @@
 package br.com.anymarket.sdk.parameter.dto;
 
 public enum MarketPlaceDelivery {
-    AMAZON("Amazon FBA", "AMAZON", "FBA_ONSITE"),
+    AMAZON("Amazon FBA Onsite", "AMAZON", "FBA_ONSITE"),
     B2W_NEW_API("B2W Entregas", "B2W_NEW_API", "B2W"),
     GFG("Dafiti Entregas", "GFG", "Dafiti"),
     MAGAZINE_LUIZA("Magalu Entregas", "MAGAZINE_LUIZA", "Magalu"),
     MERCADO_LIVRE("Mercado Envios", "MERCADO_LIVRE", "ME2"),
-    NETSHOES("Netshoes Entregas", "NETSHOES", "Netshoes");
+    NETSHOES("Netshoes Entregas", "NETSHOES", "Netshoes"),
+    AMAZON_DBA("Amazon DBA - Delivery By Amazon", "AMAZON", "DBA");
 
     private String name;
     private String marketPlace;
