2fb455facef157654fd4667f78f9243cff8788dc
Rebeca de Melo Souza <rebeca.souza@db1.com.br>
2022-11-30 18:11:19 -0300

    Product Backlog Item 339851: [BLING] [Integração de Múltiplos CNPJs] Viabilizar consulta das contas dos marketplaces para configuração de faturamento no módulo Bling

--- anymarket-sdk-marketplaces/src/main/java/br/com/anymarket/sdk/marketplaces/MarketPlacesService.java
+++ anymarket-sdk-marketplaces/src/main/java/br/com/anymarket/sdk/marketplaces/MarketPlacesService.java
@@ -1,6 +1,8 @@
 package br.com.anymarket.sdk.marketplaces;
 
+import br.com.anymarket.sdk.MarketPlace;
 import br.com.anymarket.sdk.SDKConstants;
+import br.com.anymarket.sdk.dto.MarketPlaceAccount;
 import br.com.anymarket.sdk.exception.NotFoundException;
 import br.com.anymarket.sdk.http.HttpService;
 import br.com.anymarket.sdk.http.Response;
@@ -16,6 +18,7 @@ public class MarketPlacesService extends HttpService {
 
     private static final String MARKETPLACES_URI = "/marketplaces";
     private static final String STATUS = "?status=";
+    private static final String ACCOUNTS = "/accounts";
     private final String apiEndPoint;
     private String moduleOrigin;
 
@@ -39,4 +42,12 @@ public class MarketPlacesService extends HttpService {
         throw new NotFoundException("Marketplaces not found.");
     }
 
+    public MarketPlaceAccount getActiveAccounts(MarketPlace marketPlace, IntegrationHeader... headers) {
+        GetRequest getRequest = get(apiEndPoint + MARKETPLACES_URI + "/" + marketPlace + ACCOUNTS, addModuleOriginHeader(headers, moduleOrigin));
+        Response response = execute(getRequest);
+        if (response.getStatus() == HttpStatus.SC_OK) {
+            return response.to(MarketPlaceAccount.class);
+        }
+        throw new NotFoundException("Marketplace not found.");
+    }
 }
