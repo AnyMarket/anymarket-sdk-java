617bc37750e54bbddb9279acd60c4000321d7807
eduardo.ghizoni <eduardo.ghizoni@db1.com.br>
2024-03-15 09:10:12 -0300

    Product Backlog Item 751683: [i18ANY] Disponibilizar na V2 um endpoint que permita consultar o idioma do seller pelo Oi

---
+++ anymarket-sdk-profile/src/main/java/br/com/anymarket/sdk/profile/dto/Language.java
@@ -0,0 +1,16 @@
+package br.com.anymarket.sdk.profile.dto;
+
+public class Language {
+    private String language;
+
+    public Language(){}
+
+    public Language(String language) {
+        this.language = language;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+}
