e2348effd41e7d69e7266e7fc582665e69740f7a
kaliane.viesseli <kaliane.viesseli@db1.com.br>
2024-05-10 08:43:26 -0300

    Product Backlog Item 808476: Gerenciar coluna status da entrega no módulo de importação de planilha de vendas

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/DeliveryStatus.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/DeliveryStatus.java
@@ -4,25 +4,32 @@ package br.com.anymarket.sdk.order.dto;
  * Created by gyowannyqueiroz on 5/16/16.
  */
 public enum DeliveryStatus {
-    UNKNOWN("Desconhecido"),
-    IN_TRANSIT("Em trânsito"),
-    DELIVERED("Entregue"),
-    DELAYED("Atrasado"),
-    DELIVERED_LATE("Entregue atrasado"),
-    PACKING("Em preparação"),
-    HOLD_FOR_PICKUP("Aguardando coleta"),
-    DELAYED_PICKUP("Coleta atrasada"),
-    HOLD_FOR_SHIPPED("Aguardando envio"),
-    DELAYED_SHIPPING("Envio atrasado"),
-    SHIPPED("Enviado");
+    UNKNOWN("Desconhecido", "1"),
+    IN_TRANSIT("Em trânsito", null),
+    DELIVERED("Entregue", null),
+    DELAYED("Atrasado", null),
+    DELIVERED_LATE("Entregue atrasado", null),
+    PACKING("Em preparação", "2"),
+    HOLD_FOR_PICKUP("Aguardando coleta", "3"),
+    DELAYED_PICKUP("Coleta atrasada", "4"),
+    HOLD_FOR_SHIPPED("Aguardando envio", "5"),
+    DELAYED_SHIPPING("Envio atrasado", "6"),
+    SHIPPED("Enviado", "7");
 
     private final String description;
+    private final String code;
 
-    private DeliveryStatus(String desc) {
+    private DeliveryStatus(String desc, String code) {
         description = desc;
+        this.code = code;
     }
 
     public String getDescription() {
         return description;
     }
+
+    public String getCode() {
+        return this.code;
+    }
+
 }
