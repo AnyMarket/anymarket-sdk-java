1a97e8bbfa1bee0947917a59ea1a4ccc765c60de
claudinei.geraldo <claudinei.geraldo@db1.com.br>
2024-08-02 17:38:05 -0300

    #914614-Implements title template

---
+++ anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/TitleTemplateService.java
@@ -0,0 +1,22 @@
+package br.com.anymarket.sdk.template;
+
+import br.com.anymarket.sdk.template.dto.TitleTemplate;
+
+public class TitleTemplateService extends AbstractTemplateService<TitleTemplate> {
+
+    private static final String TEMPLATE_URI = "/titleTemplate";
+
+    public TitleTemplateService(String apiEndPoint) {
+        super(TEMPLATE_URI, apiEndPoint);
+    }
+
+    public TitleTemplateService(String apiEndPoint, String origin) {
+        super(TEMPLATE_URI, apiEndPoint, origin);
+    }
+
+    @Override
+    protected Class<TitleTemplate> templateClazz() {
+        return TitleTemplate.class;
+    }
+
+}
