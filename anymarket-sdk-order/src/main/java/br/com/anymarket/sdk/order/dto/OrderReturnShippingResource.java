43d590267953c5f32aca25aefdd660cdf46ddff5
Marcia Tanimoto <marcia.tanimoto@db1.com.br>
2024-10-28 17:41:11 -0300

    [Devolução API] Incluir o novo endpoint no SDK de disponibilização da V2

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/OrderReturnShippingResource.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/OrderReturnShippingResource.java
@@ -4,9 +4,13 @@ import br.com.anymarket.sdk.serializer.SDKDateSerializer;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderReturnShippingResource {
 
