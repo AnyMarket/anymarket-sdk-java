bdd2e10e0b53d4d1e56d1454afea6d1741683546
clgeraldo <clgeraldo@gmail.com>
2025-01-13 11:19:36 -0300

    #1076850-Map to correct (List<ImageTemplateImage>) type

--- anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/ImageTemplateService.java
+++ anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/ImageTemplateService.java
@@ -3,12 +3,15 @@ package br.com.anymarket.sdk.template;
 import br.com.anymarket.sdk.http.headers.IntegrationHeader;
 import br.com.anymarket.sdk.template.dto.ImageTemplate;
 import br.com.anymarket.sdk.template.dto.ImageTemplateImage;
+import br.com.anymarket.sdk.util.MapTo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
 public class ImageTemplateService extends AbstractTemplateService<ImageTemplate> {
 
     private static final String TEMPLATE_URI = "/imageTemplates";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     public ImageTemplateService(String apiEndPoint) {
         super(TEMPLATE_URI, apiEndPoint);
@@ -25,8 +28,10 @@ public class ImageTemplateService extends AbstractTemplateService<ImageTemplate>
 
     public List<ImageTemplateImage> getImages(Long  imageTemplateId, IntegrationHeader... headers) {
         String path = String.format("/%s/images", imageTemplateId);
-        return getList(path, "Images", headers);
+        List<ImageTemplateImage> images = getList(path, "Images", headers);
+        // An ArrayList of LinkedHashMap is returned from the getList. That's why this map is needed.
+        Class<ImageTemplateImage> type = ImageTemplateImage.class;
+        return MapTo.map(MAPPER, type, images);
     }
-
 
 }
