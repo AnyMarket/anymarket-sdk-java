db6a0f821bbc758ec07aa7ec357d0343a6446fdf
clgeraldo <clgeraldo@gmail.com>
2025-01-14 07:59:19 -0300

    #1076850-Add attributes in ImageTemplateImage

--- anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/dto/ImageTemplateImage.java
+++ anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/dto/ImageTemplateImage.java
@@ -1,5 +1,6 @@
 package br.com.anymarket.sdk.template.dto;
 
+import br.com.anymarket.sdk.dto.ImageStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
@@ -17,5 +18,23 @@ public class ImageTemplateImage implements Serializable {
 
     @JsonProperty("standardUrl")
     private URL standardUrl;
+
+    @JsonProperty("originalImage")
+    private URL originalImageInS3URL;
+
+    @JsonProperty("status")
+    private ImageStatus status;
+
+    @JsonProperty("standardWidth")
+    private Integer standardWidth;
+
+    @JsonProperty("standardHeight")
+    private Integer standardHeight;
+
+    @JsonProperty("originalWidth")
+    private Integer originalWidth;
+
+    @JsonProperty("originalHeight")
+    private Integer originalHeight;
 
 }
