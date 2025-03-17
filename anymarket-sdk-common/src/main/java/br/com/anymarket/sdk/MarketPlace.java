1b6205eeec78d559443e26946c6d096915206f39
Gitlab Runner <jenkins@anymarket.com.br>
2025-03-17 17:32:18 +0000

    New Marketplace TIKTOK_SHOP

--- anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/MarketPlace.java
+++ anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/MarketPlace.java
@@ -196,7 +196,8 @@ public enum MarketPlace {
     DIGIGROW("Digigrow"),
     VOCE("Você"),
     PNEUBEST("PneuBest"),
-    DB1_MARKETPLACE("DB1 Marketplace");
+    DB1_MARKETPLACE("DB1 Marketplace"),
+    TIKTOK_SHOP("TIKTOK SHOP");
 
     private String name;
 
