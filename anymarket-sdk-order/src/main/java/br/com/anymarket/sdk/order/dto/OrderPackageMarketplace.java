c1950b8a8e5b9658bd77fca08e6973c045f86fa7
Dieyson Gustavo da Silva <dieyson.silva@db1.com.br>
2026-05-27 19:41:48 -0300

    feat(order-package)Implement Serializable interface for OrderPackageMarketplace class

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/OrderPackageMarketplace.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/OrderPackageMarketplace.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderPackageMarketplace {
+public class OrderPackageMarketplace implements Serializable {
 
     private Long orderId;
     private Long packageId;
